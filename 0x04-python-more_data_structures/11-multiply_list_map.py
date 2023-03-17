43a03701beae585a3064663d4e6a687b17751fef
NneomaObi <obidegwu.nneoma@yahoo.com>
2023-03-16 17:49:00 -0700

    A

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 2-uniq_add.py
-# Nneoma Obidegwu <obidegwu.nneoma@yahoo.com>
 
 def mutiply_list_map(my_list=[], number=0):
     return (list(map(lambda x: x*number, my_list)))
