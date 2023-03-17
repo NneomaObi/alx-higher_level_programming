545b322a00553901a4fc9ec4c2348c5e1c87bbf3
NneomaObi <obidegwu.nneoma@yahoo.com>
2023-03-16 17:28:52 -0700

    A function that returns a set of common elements in two sets.

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+# Nneoma Obidegwu <obidegwu.nneoma@yahoo.com>
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
