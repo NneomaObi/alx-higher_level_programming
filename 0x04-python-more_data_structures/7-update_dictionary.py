76bee92613b51e625ef8234ecdf47e2e8d9998d1
NneomaObi <obidegwu.nneoma@yahoo.com>
2023-03-16 17:37:42 -0700

    A function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+# Nneoma Obidegwu <obidegwu.nneoma@yahoo.com>
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
