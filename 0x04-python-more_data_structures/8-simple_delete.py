d679543e677727cfd02ecf3f0d571a8c01c3cc24
NneomaObi <obidegwu.nneoma@yahoo.com>
2023-03-16 17:39:35 -0700

    A function that deletes a key in a dictionary.

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+# Nneoma Obidegwu <obidegwu.nneoma@yahoo.com>
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
